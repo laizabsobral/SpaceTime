a0315455fe0171a89447ca75ffa641f113c4d661
laiza <laiza.b.sobral@accenture.com>
2023-05-17 15:31:41 -0300

    add mobile

---
+++ aulas/mobile/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="bg-gray-950">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
