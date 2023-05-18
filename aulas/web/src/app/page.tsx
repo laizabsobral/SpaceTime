7f5155cb77b7706bcbb418614422cbcaa8a0657c
laiza <laiza.b.sobral@accenture.com>
2023-05-18 12:08:06 -0300

    add fonts

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50">
-      <h1> Sua cápsula do tempo </h1>
+      <h1 className="font-alt"> Sua cápsula do tempo </h1>
+      <h1 className="font-bold"> Sua cápsula do tempo </h1>
     </div>
   );
 }
