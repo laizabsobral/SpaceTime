9f84e29eacfc71a6b330f5af64db12b2eade2041
laiza <laiza.b.sobral@accenture.com>
2023-05-18 13:48:45 -0300

    add blur effect

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <div className="h-screen bg-zinc-950 text-zinc-50"></div>;
+  return (
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden">
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
+      </div>
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            {" "}
+            Você ainda não registrou nenhuma lembrança, comece a {""}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!{" "}
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
