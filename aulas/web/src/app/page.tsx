0a93905ee1de597ee9c17c1256fced71a0bdeab8
laiza <laiza.b.sobral@accenture.com>
2023-05-18 14:05:25 -0300

    upd sign in

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -9,10 +9,20 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute right-1 top-0 bottom-0 w-2 bg-stripes "> </div>
 
-        <a href="" className="flex items-center gap-3 text-left">
+        {/* Sign in */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors"
+        >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500"> </User>
           </div>
+
+          <p className="text-sm leading-snug max-w-[140px]">
+            {" "}
+            <span className="underline">Crie sua conta </span> e salve suas
+            memórias!
+          </p>
         </a>
       </div>
 
