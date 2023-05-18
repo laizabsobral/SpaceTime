247ef88fce8c8fd8cba27c3ff116adebb7b31b84
laiza <laiza.b.sobral@accenture.com>
2023-05-18 14:02:09 -0300

    add icon user

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { User } from "lucide-react";
 export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
@@ -7,7 +8,14 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
         {/* Stripes */}
         <div className="absolute right-1 top-0 bottom-0 w-2 bg-stripes "> </div>
+
+        <a href="" className="flex items-center gap-3 text-left">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500"> </User>
+          </div>
+        </a>
       </div>
+
       {/* Right */}
       <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
